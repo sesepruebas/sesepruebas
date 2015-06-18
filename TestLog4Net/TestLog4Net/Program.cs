eed15c7b7756daa141adbabb12a851b8f1b85760
Saúl Correas Subías <saul.correas@gmail.com>
2015-06-18 15:07:13 +0200

    Versión inicial de prueba

---
+++ TestLog4Net/TestLog4Net/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLog4Net
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo(@"c:\local\etc\log4net.config"));
+            log.Info("-----> Inicio");
+
+
+
+            log.Info("<----- Fin");
+        }
+    }
+}
